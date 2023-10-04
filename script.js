49a843e63b5f0822f08232a94ce12be75f024b83
Ashtacon <astacialvndr@gmail.com>
2023-10-04 09:12:58 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+gsap.from(".title", {opacity: 0, delay: .5, duration: 1});
+gsap.from(".subtitle", {opacity: 0, delay: 1, duration: 1});
+gsap.from(".hero-image", {left:-500, delay: 1, duration: 1});
+gsap.from("#hero", {opacity: 0, delay: 1, duration: 1});
+
